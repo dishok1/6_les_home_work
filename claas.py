9d81fc5da1b8c0d44602053c7248fb4119eb08a0
Dima Pan <dimdish4@gmail.com>
2026-07-02 11:40:46 +0300

    first commit

---
+++ claas.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self, name, species, age):
+        self.name = name
+        self.species = species
+        self.age = age
+
+    
+    def make_sound(self):
+        if self.species.lower() == "cat":
+            print(f"{self.name} says: Meow!")
+        elif self.species.lower() == "dog":
+            print(f"{self.name} says: Woof!")
+        else:
+            print(f"{self.name} makes an unknown sound.")
+
+cat = Animal(name="Whiskers", species="cat", age=3)
+
+dog = Animal(name="Buddy", species="dog", age=5)
+
+cat.make_sound()
+dog.make_sound()
---
+++ object.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+rect1 = Rectangle(width=7, height=100)
+
+rect2 = Rectangle(width=2.3, height=4)
+
+area1 = rect1.calculate_area()
+area2 = rect2.calculate_area()
+
+print(f"The area of the first rectangle is: {area1}")
+print(f"The area of the second rectangle is: {area2}")
